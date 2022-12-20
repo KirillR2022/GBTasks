f613dd9c3496988f41986c4a8a92542558d60284
Kirill <kirill075@mail.ru>
2022-12-20 14:46:18 +0300

    Task2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+WriteLine("Является ли число a квадратом числа b?");
+
+Write("Введите число а: ");
+int a = int.Parse(ReadLine());
+Write("Введите число b: ");
+int b = int.Parse(Readline());
+   
+if (b * b == a)
+{
+    WriteLine("Да");
+}   
+else
+{
+    WriteLine("Нет");
+}
